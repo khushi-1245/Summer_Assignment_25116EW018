3dbb8f286ee805ec5a082c05fa63999f79c5ac48
khushi <poonambaba5678@gmail.com>
2026-06-05 08:36:42 +0530

    Added DAY-5 folder

---
+++ DAY- 5/20.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    scanf("%d", &n);
+
+    while (n % 2 == 0) {
+        n = n / 2;
+    }
+
+    for (i = 3; i * i <= n; i = i + 2) {
+        while (n % i == 0) {
+            n = n / i;
+        }
+    }
+
+    if (n > 2) {
+        printf("%d", n);
+    }
+
+    return 0;
+}
