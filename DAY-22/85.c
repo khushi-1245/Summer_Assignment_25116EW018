8b6ebe988eaf86a28818fc0a43f4369b510ae2a3
khushi <poonambaba5678@gmail.com>
2026-06-22 10:38:00 +0530

    Added DAY 22

---
+++ DAY-22/85.c
@@ -0,0 +1,31 @@
+ #include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, len = 0, flag = 1;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    while(str[len] != '\0')
+    {
+        len++;
+    }
+
+    for(i = 0; i < len / 2; i++)
+    {
+        if(str[i] != str[len - 1 - i])
+        {
+            flag = 0;
+            break;
+        }
+    }
+
+    if(flag == 1)
+        printf("String is Palindrome");
+    else
+        printf("String is Not Palindrome");
+
+    return 0;
+}
